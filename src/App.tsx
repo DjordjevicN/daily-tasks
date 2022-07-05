9b27c6ecebf27aa3c3199bc37b864d77baeed4e7
Nikola <nikola.dj.87@gmail.com>
2022-07-05 22:27:25 +0200

    adding done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import InputField from "./compornents/inputField/ImputField";
+import { Task } from "./model";
 
 const App: React.FC = () => {
   const [task, setTask] = useState<string>("");
+  const [allTasks, setAllTasks] = useState<Task[]>([]);
+  console.log(allTasks);
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (task) {
+      setAllTasks([...allTasks, { id: Date.now(), task, isDone: false }]);
+    }
+  };
 
   return (
     <div className="App">
       <div className="heading">
         <h2>Daily tasks</h2>
       </div>
-      <InputField task={task} setTask={setTask} />
+      <InputField task={task} setTask={setTask} handleAdd={handleAdd} />
     </div>
   );
 };
--- src/compornents/inputField/ImputField.tsx
+++ src/compornents/inputField/ImputField.tsx
@@ -4,11 +4,18 @@ import "./inputField.scss";
 interface Props {
   task: string;
   setTask: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
-const InputField: React.FC<Props> = ({ task, setTask }) => {
+const InputField: React.FC<Props> = ({ task, setTask, handleAdd }) => {
   return (
-    <form className="input">
-      <input className="input__box" type="text" placeholder="Enter new task" />
+    <form className="input" onSubmit={handleAdd}>
+      <input
+        onChange={(e) => setTask(e.target.value)}
+        value={task}
+        className="input__box"
+        type="text"
+        placeholder="Enter new task"
+      />
       <button className="input__submit" type="submit">
         Add
       </button>
